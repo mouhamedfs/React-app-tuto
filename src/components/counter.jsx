7908b8e10772959946fb95ab2dcf3a5ec0b06e32
mouha10 <mouha10fs@gmail.com>
2020-11-05 14:23:26 +0000

    Mosh app

---
+++ src/components/counter.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 1,
+    tags: [],
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There is no tags !!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && <p>Please enter a tags</p>}
+        {this.renderTags()}
+      </div>
+    );
+  }
+}
+
+export default Counter;
